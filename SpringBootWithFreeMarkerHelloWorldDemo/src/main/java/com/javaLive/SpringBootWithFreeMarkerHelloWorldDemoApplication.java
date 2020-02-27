8b729497b5f03297b4f37f09df84bc728358aeb5
Administrator <Administrator@DELL>
2020-02-27 18:49:08 +0530

    Initial commit.

---
+++ SpringBootWithFreeMarkerHelloWorldDemo/src/main/java/com/javaLive/SpringBootWithFreeMarkerHelloWorldDemoApplication.java
@@ -0,0 +1,13 @@
+package com.javaLive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithFreeMarkerHelloWorldDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithFreeMarkerHelloWorldDemoApplication.class, args);
+	}
+
+}
---
+++ SpringBootWithFreeMarkerHelloWorldDemo/src/main/webapp/welcome.ftl
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Java Live!!!</title>
+</head>
+<body>
+    <h2>Welcome to Java Live!!!</h2>
+</body>
+</html>
